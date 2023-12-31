525b32b8326e27f68209b090492786600e3c29f0
pavellinkevich85 <plpce@mail.ru>
2023-12-31 19:13:25 +0300

    tasks

--- list_in_list.py
+++ list_in_list.py
@@ -1,4 +1,14 @@
 N = int(input('Кол-во участников: '))
-members = list(range(1, N + 1))
-print(members)
+members = []
+
+num = 1
+for _ in range(N // 3):
+    members.append(list(range(num, num + 3)))
+    num += 3
+#print(members)
+
+for i_team in members:
+    for man in i_team:
+        print(man, end = ' ')
+    print('\n')
 
--- text_1.py
+++ text_1.py
@@ -3,19 +3,34 @@
 #     list.append(i ** 2)
 # print(list)
 
-list = [i * 2 for i in 'abc' ]
-print(list)
+# list = [i * 2 for i in 'abc' ]
+# print(list)
+#
+# list = [i ** 2 for i in range(1, 10 + 1)]
+# print(list)
+#
+# list = [i ** 3 for i in range(10, -1, -1)]
+# print(list)
+#
+# prices_now = [1.09, 23.56, 57.84, 4.56, 6.78]
 
-list = [i ** 2 for i in range(1, 10 + 1)]
-print(list)
 
-list = [i ** 3 for i in range(10, -1, -1)]
-print(list)
+N = int(input("Кол-во человек: "))
+K = int(input("Какое число в считалке? "))
+print("Значит, выбывает каждый", K, "человек.")
+people = list(range(1, N+1))
+out = 0
 
-prices_now = [1.09, 23.56, 57.84, 4.56, 6.78]
+for i in range(N-1):
+    print('Текущий круг людей', people)
+    start_count = out % len(people)
+    out = (start_count + K - 1) % len(people)
+    print('Начало счёта с номера', people[start_count])
+    print('Выбывает человек под номером', people[out])
+    people.remove(people[out])
+    print()
 
-
-
+print('Остался человек под номером', people[0])
 
 
 
